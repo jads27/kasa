b51e086096abe7ca60eb89bdb7601fab142336bd
jads27 <jalvesdsilva27@gmail.com>
2023-09-13 18:57:14 +0200

    Formatting CardDetails and adding components

--- src/components/CardDetails.jsx
+++ src/components/CardDetails.jsx
@@ -5,6 +5,8 @@ import Header from "./Header";
 import Footer from "./Footer";
 import SlideShow from "./SlideShow";
 import Collapse from "./Collapse";
+import Rating from "./Rating";
+import Error from "./Error"
 
 const CardDetails = () => {
     const {id} = useParams();
@@ -12,40 +14,57 @@ const CardDetails = () => {
     const [loading, setLoading] = useState(true);
 
     useEffect(() => {
-        axios.get("logements.json").then((res) => {
+        axios.get("logements.json")
+        .then((res) => {
             const data = res.data;
             const card = data.find((card) => card.id === id);
             card ? setCardData(card) : "";
-            setLoading(false);
-        });
+            setLoading(false)
+        })
+        .catch ((error) => {
+            console.error("Error fetching JSON data", error)
+        })
     }, [id]);
 
     return (
         <div>
             <Header />
-            <main>
+            <main className="card-details">
                 {!loading ? (
-                    <div>
-                        <img src={cardData.pictures[0]} />
-                        <h1>{cardData.title}</h1>
-                        <p>{cardData.host.name}</p>
-                        <img src={cardData.host.picture} />
-                        <p>{cardData.location}</p>
-                        {cardData.tags.map((card, index) => (
-                            <p key={index}>{card}</p>
-                        ))}
-                        <p>{cardData.rating}</p>
-                        <Collapse title="Description" content={cardData.description} />
-                        <Collapse
-                            title="Équipements"
-                            content={cardData.equipments.map((equipment, index) => (
-                                <span key={index}>{equipment}</span>
-                            ))}
-                        />
-                    </div>
-                ) : (
-                    ""
-                )}
+                    <React.StrictMode>
+                        <SlideShow image={cardData.pictures} alt={cardData.title} />
+                        <div className="content-wrapper-1">
+                            <div className="title-content">
+                                <h1>{cardData.title}</h1>
+                                <p>{cardData.location}</p>
+                            </div>
+                            <div className="host-content">
+                                <div>
+                                    <span>{cardData.host.name.split(" ")[0]}</span>
+                                    <span>{cardData.host.name.split(" ")[1]}</span>
+                                </div>
+                                <img src={cardData.host.picture} />
+                            </div>
+                        </div>
+                        <div className="content-wrapper-2">
+                            <div className="tag-content">
+                                {cardData.tags.map((tag, index) => (
+                                    <span key={index}>{tag}</span>
+                                ))}
+                            </div>
+                            <Rating rating={parseFloat(cardData.rating)}/>
+                        </div>
+                        <div className="content-wrapper-3">
+                            <Collapse title="Description" content={cardData.description} />
+                            <Collapse
+                                title="Équipements"
+                                content={cardData.equipments.map((equipment, index) => (
+                                    <span key={index}>{equipment}</span>
+                                ))}
+                            />
+                        </div>
+                    </React.StrictMode>
+                ) : <Error />}
             </main>
             <Footer />
         </div>
