a44fcc0aad74607f8be48a3d516a8471628bf0ee
jads27 <jalvesdsilva27@gmail.com>
2023-09-13 18:55:26 +0200

    Edit settings style and Footer text

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -4,7 +4,7 @@ const Footer = () => {
     return (
         <footer>
             <img src="logo-white.png" />
-            <p>© 2020 Kasa. All rights reserved</p>
+            <p>© 2023 Kasa. All rights reserved</p>
         </footer>
     );
 };
