72bcfefbb292733d429223c7caf2ec4b4b29654c
jads27 <jalvesdsilva27@gmail.com>
2023-09-15 09:04:31 +0200

    Edit name and file path of Error component

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import {BrowserRouter as Router, Routes, Route} from "react-router-dom";
 import Home from "./pages/Home";
 import About from "./pages/About";
-import Error from "./components/Error"
+import Error from "./pages/Error";
 import CardDetails from "./components/CardDetails";
 
 function App() {
---
+++ src/pages/Error.jsx
@@ -0,0 +1,20 @@
+import React from "react";
+import {NavLink} from "react-router-dom";
+import Header from "../components/Header";
+import Footer from "../components/Footer";
+
+const Error = () => {
+    return (
+        <div>
+            <Header />
+            <main className="error">
+                <p>404</p>
+                <p>Oups ! La page que vous demandez n'existe pas.</p>
+                <NavLink to="/">Retournez sur la page d'accueil</NavLink>
+            </main>
+            <Footer />
+        </div>
+    );
+};
+
+export default Error;
